d9b38cdb27ea958267d6feb6fad2cd00463cec89
Scott Leung <371569963@qq.com>
2020-12-23 12:26:11 +0800

    [U] LeetCode-387-增加多种做法

--- LeetCode/387-字符串中的第一个唯一字符【Easy】.js
+++ LeetCode/387-字符串中的第一个唯一字符【Easy】.js
@@ -24,27 +24,62 @@
 //   return -1;
 // };
 
+// 做法二：in标识来优化
+// var firstUniqChar = function (s) {
+//   let hash = {};
+//   let queue = [];
+//   for (let i = 0; i < s.length; i++) {
+//     const c = s[i];
+//     if (hash[c]) {
+//       if (hash[c].in) {
+//         let index = queue.indexOf(c);
+//         queue.splice(index, 1);
+//         hash[c].in = false;
+//       }
+//     } else {
+//       hash[c] = {
+//         pos: i,
+//         in: true
+//       }
+//       queue.push(c);
+//     }
+//   }
+//   return queue[0] ? hash[queue[0]].pos : -1;
+// };
+
+// 做法三：延迟删除优化
+// var firstUniqChar = function (s) {
+//   let hash = {};
+//   let queue = [];
+//   const length = s.length;
+//   for (let i = 0; i < length; i++) {
+//     const c = s[i];
+//     // console.log(hash);
+//     if (hash[c] >= -1) {
+//       hash[c] = -1;
+//       while (queue.length && hash[queue[0]] === -1) {
+//         queue.shift();
+//       }
+//     } else {
+//       hash[c] = i;
+//       queue.push(c);
+//     }
+//   }
+//   return queue[0] ? hash[queue[0]] : -1;
+// };
+
+// 做法四：前后索引相等
 var firstUniqChar = function (s) {
+  const length = s.length;
   let hash = {};
-  let inQueue = {};
-  let queue = [];
-  for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < length; i++) {
     const c = s[i];
-    if (hash[c]) {
-      if (hash[c].in) {
-        let index = queue.indexOf(c);
-        queue.splice(index, 1);
-        hash[c].in = false;
-      }
-    } else {
-      hash[c] = {
-        pos: i,
-        in: true
-      }
-      queue.push(c);
+    if (!hash[c]) {
+      if (s.lastIndexOf(c) === s.indexOf(c)) return i;
+      hash[c] = true;
     }
   }
-  return queue[0] ? hash[queue[0]].pos : -1;
+  return -1;
 };
 
 console.log(firstUniqChar('leetcode'));
