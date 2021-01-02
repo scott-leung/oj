4aaeb5848f83d5c31530e62940ad2bc51f7f6e60
Scott Leung <371569963@qq.com>
2021-01-02 23:59:08 +0800

    [U] LeetCode-239-更新代码思路

--- LeetCode/239-滑动窗口最大值【Hard】.js
+++ LeetCode/239-滑动窗口最大值【Hard】.js
@@ -1,5 +1,9 @@
 // 题目链接：https://leetcode-cn.com/problems/sliding-window-maximum/
 
+// 思路：利用严格单调队列，先求初始队列情况
+// 然后对于每个值，判定滑出的值是否等于单调队列队首的值，如果是则弹出队首
+// 同理，需要保证滑入的值插入单调队列时保证队列的严格单调即可。
+
 /**
  * @param {number[]} nums
  * @param {number} k
