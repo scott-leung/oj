bebb0e0e56e23a8e4d9c00bea413625500561c9b
Scott Leung <371569963@qq.com>
2021-01-07 11:51:22 +0800

    [A] LeetCode-547-每日一题打卡完成~

---
+++ LeetCode/547-省份数量【Medium】.js
@@ -0,0 +1,39 @@
+// 题目链接：https://leetcode-cn.com/problems/number-of-provinces/
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+  let sum = 0;
+  for (let i = 0; i < isConnected.length; i++) {
+    for (let j = 0; j < isConnected[i].length; j++) {
+      const item = isConnected[i][j];
+      if (item) {
+        sum++;
+        dfsClean(isConnected, i, j);
+      }
+    }
+  }
+  return sum;
+};
+
+function dfsClean(arr, i, j) {
+  if (!arr[i][j]) return ;
+  arr[i][j] = 0;
+  arr[j][i] = 0;
+  for (let k = 0; k < arr.length; k++) {
+    if (arr[i][k]) dfsClean(arr, i, k);
+    if (arr[j][k]) dfsClean(arr, j, k);
+  }
+}
+
+console.log(findCircleNum([[1,1,0],[1,1,0],[0,0,1]]));
+console.log(findCircleNum([[1,0,0],[0,1,0],[0,0,1]]));
+console.log(findCircleNum([
+  [1,0,0,1],
+  [0,1,1,0],
+  [0,1,1,1],
+  [1,0,1,1]
+]));
+
