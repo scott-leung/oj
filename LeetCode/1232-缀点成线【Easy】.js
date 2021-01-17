60923119a2a413a686f7f1b24e3b6781eeb50c7e
Scott Leung <371569963@qq.com>
2021-01-17 21:01:16 +0800

    [A] LeetCode-1232-每日一题打卡完成~！

---
+++ LeetCode/1232-缀点成线【Easy】.js
@@ -0,0 +1,22 @@
+// 题目链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/
+
+// (x3-x1)*(y2-y1) = (y3-y1) * (x2-x1)
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function (coordinates) {
+  const [x1, y1] = coordinates[0];
+  const [x2, y2] = coordinates[1];
+  const a = (y2 - y1);
+  const b = (x2 - x1);
+  for (let i = 0; i < coordinates.length; i++) {
+    const [x3, y3] = coordinates[i];
+    if ((x3 - x1) * a !== (y3 - y1) * b) return false;
+  }
+
+  return true;
+};
+
+console.log(checkStraightLine([[1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 7]]));
+console.log(checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]]));
