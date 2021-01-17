4a040848a2930b3160b9b4f5c74d4e1265b6e163
Scott Leung <371569963@qq.com>
2021-01-17 21:02:06 +0800

    [U] LeetCode-1232-思路更新

--- LeetCode/1232-缀点成线【Easy】.js
+++ LeetCode/1232-缀点成线【Easy】.js
@@ -1,6 +1,6 @@
 // 题目链接：https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/
 
-// (x3-x1)*(y2-y1) = (y3-y1) * (x2-x1)
+// 思路：套用两点一线公式 (x3-x1)*(y2-y1) = (y3-y1) * (x2-x1)
 /**
  * @param {number[][]} coordinates
  * @return {boolean}
@@ -10,7 +10,7 @@ var checkStraightLine = function (coordinates) {
   const [x2, y2] = coordinates[1];
   const a = (y2 - y1);
   const b = (x2 - x1);
-  for (let i = 0; i < coordinates.length; i++) {
+  for (let i = 3; i < coordinates.length; i++) {
     const [x3, y3] = coordinates[i];
     if ((x3 - x1) * a !== (y3 - y1) * b) return false;
   }
